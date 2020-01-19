7bd51464dc8db85e252200711f74462e9f83a57b
SumanX22 <suman.mesaha22@gmail.com>
2020-01-19 23:00:36 +0530

    1223

--- models/userLocation.js
+++ models/userLocation.js
@@ -1,8 +1,7 @@
 const mongoose = require('mongoose');
 const userLocationSchema = mongoose.Schema({
-	_id: mongoose.Schema.Types.ObjectId,
 	name: String,
 	latitude: Number,
 	longitude: Number
 });
-module.exports = mongoose.model('userLocation', userLocationSchema);
+module.exports =  mongoose.models.userLocation || mongoose.model('userLocation', userLocationSchema);
